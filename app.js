d6e23464c1552a30f2dbc569928084b695ca301c
piomar123 <piomar123@gmail.com>
2017-01-27 22:12:24 +0100

    Created EJS common template for all subpages. Navbar active page highlight.

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 var express = require("express");
 var app = express();
+var aws = require('aws-sdk');
 var utils = require("./utils");
+
+var AWS_CONFIG_FILE = "config.json";
+var POLICY_FILE = "policy.json";
 var PORT = 8080;
+
+aws.config.loadFromPath(AWS_CONFIG_FILE);
 
 app.set("view engine", "ejs");
 app.disable("view cache");
 app.use("/static", express.static("views/static", { fallthrough:false, index:false }));
+
 app.get("/", function(req, res) {
-    res.render("index");
+  res.render("index", { ejs: { view: "upload", title: "Upload image" } });
+});
+app.get("/logs", function(req, res) {
+  setTimeout(function(){
+    res.render("index", { ejs: { view: "logs", title: "Logs" },
+      entries: [{
+        timestamp: "1234",
+        level: "low",
+        message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        other: "Hello world!"
+      }] });
+  }, 500);  // simulate async requests
 });
 
 app.listen(PORT);
