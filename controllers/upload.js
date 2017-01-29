83676af4ff1b6228ee13323ca9ef50e76c7942ab
piomar123 <piomar123@gmail.com>
2017-01-29 01:54:41 +0100

    Requesting thumbnail to SQS.

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,5 +1,6 @@
-//var aws = require("aws-sdk");
+var aws = require("aws-sdk");
 var AwsS3Form = require("aws-s3-form");
+var sqs = new aws.SQS();
 var fs = require("fs");
 var _ = require("lodash");
 var logger = require("../logic/logger");
@@ -8,14 +9,19 @@ var AWS_CONFIG_FILE = "./config.json";
 var S3_BUCKET = "lab4-weeia";
 var S3_KEY_PREFIX = "piotr.marcinczyk/project/upload/";
 
+var SQS_MESSAGE_TEXT = "Request to create image thumbnail.";
+var SQS_URL = "https://sqs.us-west-2.amazonaws.com/983680736795/MarcinczykSQS";
+var SQS_MSG_ID = "S3projSQS";
+var ACTION_THUMBNAIL = "thumbnail";
+
 if(!fs.existsSync(AWS_CONFIG_FILE)) {
 	throw new Error("Unable to read config file: " + AWS_CONFIG_FILE);
 }
 var awsConfig = JSON.parse(fs.readFileSync(AWS_CONFIG_FILE, { encoding:"utf8" }));
 
-exports.showUploadForm = function(request, res, uploaded) {
+exports.showUploadForm = function(request, res) {
   var successFilename = null;
-  if(uploaded === true){
+  if(request.uploadSuccess === true){
     var filename = _.last(request.query.key.split("/"));
     successFilename = filename;
   } else {
@@ -53,7 +59,7 @@ exports.showUploadForm = function(request, res, uploaded) {
   });
 };
 
-exports.s3uploadDone = function(request, res){
+exports.s3uploadDone = function(request, res, next){
   logger.info("S3 file uploaded", {
     client: request.connection.remoteAddress,
     host: request.hostname,
@@ -62,6 +68,32 @@ exports.s3uploadDone = function(request, res){
   });
 
   console.log(request.query);
-  exports.showUploadForm(request, res, true);
+
+  var afterSentToSQS = function(err){
+    if(err){
+      next(err);
+    }
+    var logData = {
+      client: request.connection.remoteAddress,
+      host: request.hostname,
+      s3bucket: request.query.bucket,
+      s3key: request.query.key
+    };
+    logger.info("Uploaded image thumbnail request to SQS", logData);
+    request.uploadSuccess = true;
+    exports.showUploadForm(request, res, next);
+  };
+
+  sqs.sendMessage(
+    {
+      MessageBody: SQS_MESSAGE_TEXT,
+      QueueUrl: SQS_URL,
+      MessageAttributes: {
+        id: { DataType: "String", StringValue: SQS_MSG_ID },
+        s3bucket: { DataType: "String", StringValue: request.query.bucket },
+        s3key: { DataType: "String", StringValue: request.query.key },
+        action: { DataType: "String", StringValue: ACTION_THUMBNAIL }
+      }
+    }, afterSentToSQS);
 };
 
