11fdc884975fe3ddbc1f175ab1a73688eb3f1658
piomar123 <piomar123@gmail.com>
2017-01-30 01:59:12 +0100

    Module and host in logs. Thumbnails from S3 in gallery controller.

--- controllers/images.js
+++ controllers/images.js
@@ -6,7 +6,7 @@ var CONFIG = require("../config");
 exports.showGallery = function(request, res, next){
   s3.listObjectsV2({
     Bucket: CONFIG.S3_BUCKET,
-    Prefix: CONFIG.S3_KEY_PREFIX_UPLOAD
+    Prefix: CONFIG.S3_KEY_PREFIX_THUMBS
   }, afterListedObjects);
 
   function afterListedObjects(err, data){
@@ -14,15 +14,16 @@ exports.showGallery = function(request, res, next){
       next(err);
     }
     console.log(data.Contents);
-    var obj = data.Contents[2];
-    var filename = _.last(obj.Key.split("/"));
-    var url = s3.getSignedUrl("getObject", {
-      Bucket: CONFIG.S3_BUCKET,
-      Key: obj.Key
+    var thumbs = [];
+    data.Contents.forEach(function(obj){
+      var filename = _.last(obj.Key.split("/"));
+      if(filename === "") return;
+      var url = s3.getSignedUrl("getObject", {
+        Bucket: CONFIG.S3_BUCKET,
+        Key: obj.Key
+      });
+      thumbs.push({ name: filename, url: url });
     });
-    var thumbs = [
-      { name: filename, url: url }
-    ];
     res.render("index", {
       ejs: { view: "gallery", title: "Gallery" },
       thumbs: thumbs
